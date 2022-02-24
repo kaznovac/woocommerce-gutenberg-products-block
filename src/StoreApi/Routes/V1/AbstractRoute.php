2c0fdcb3fd08c460b8387be2c169e410ed925941
“Chris <xristos3490@gmail.com>
2022-02-24 14:44:16 +0200

    Fix namespace versioning and some minor improvements

--- src/StoreApi/Routes/V1/AbstractRoute.php
+++ src/StoreApi/Routes/V1/AbstractRoute.php
@@ -1,11 +1,9 @@
 <?php
 namespace Automattic\WooCommerce\Blocks\StoreApi\Routes\V1;
 
-use Automattic\WooCommerce\Blocks\StoreApi\Utilities\InvalidCartException;
 use Automattic\WooCommerce\Blocks\StoreApi\SchemaController;
 use Automattic\WooCommerce\Blocks\StoreApi\Routes\RouteInterface;
 use Automattic\WooCommerce\Blocks\StoreApi\Exceptions\RouteException;
-use Automattic\WooCommerce\Blocks\StoreApi\Exceptions\InvalidStockLevelsInCartException;
 use Automattic\WooCommerce\Blocks\StoreApi\Schemas\v1\AbstractSchema;
 use WP_Error;
 
@@ -114,8 +112,6 @@ abstract class AbstractRoute implements RouteInterface {
 			}
 		} catch ( RouteException $error ) {
 			$response = $this->get_route_error_response( $error->getErrorCode(), $error->getMessage(), $error->getCode(), $error->getAdditionalData() );
-		} catch ( InvalidCartException $error ) {
-			$response = $this->get_route_error_response_from_object( $error->getError(), $error->getCode(), $error->getAdditionalData() );
 		} catch ( \Exception $error ) {
 			$response = $this->get_route_error_response( 'unknown_server_error', $error->getMessage(), 500 );
 		}
